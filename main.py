4ac9f9d65832ac460b7341b45095d1c76a51674f
MaryamTaj <109046900+MaryamTaj@users.noreply.github.com>
2023-03-22 19:44:54 -0400

    Maryam

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ Copyright and Usage Information
 This file is Copyright (c) 2023 Akshaya D., Kashish M., Maryam T. and Pratibha T..
 """
 
-from python_ta.contracts import check_contracts
+# from python_ta.contracts import check_contracts
+import csv
+import snscrape.modules.twitter as sntwitter
+
+# Define the query to search for
+query = 'python'
+
+# Define the url to search
+url = 'https://twitter.com/search?q={}&lang=en'.format(query)
+
+# Create a csv file to write the results to
+with open('tweets.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    writer = csv.writer(csv_file)
+
+    # Iterate over the tweets matching the query
+    for tweet in sntwitter.TwitterSearchScraper(url).get_items():
+        writer.writerow([tweet.date, tweet.content, tweet.followersCount])
 
 if __name__ == '__main__':
     import doctest
