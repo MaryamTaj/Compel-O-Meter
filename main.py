2f9912ee63a25e4421615aa3f6c3500045e914b3
MaryamTaj <109046900+MaryamTaj@users.noreply.github.com>
2023-03-22 19:26:29 -0400

    Maryam

---
+++ main.py
@@ -0,0 +1,6 @@
+"""CSC111 Winter 2023 Assignment 4: Compel-O-Meter
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2023 Akshaya D., Kashish M., Maryam T. and Pratibha T..
+"""
