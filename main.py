ac4f76a8529391b8d1800add7b4b95a1012e4f2a
MaryamTaj <109046900+MaryamTaj@users.noreply.github.com>
2023-03-22 19:28:25 -0400

    Misc.

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ Copyright and Usage Information
 ===============================
 This file is Copyright (c) 2023 Akshaya D., Kashish M., Maryam T. and Pratibha T..
 """
+
+from python_ta.contracts import check_contracts
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=True)
+
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (In PyCharm, select the lines below and press Ctrl/Cmd + / to toggle comments.)
+    # You can use "Run file in Python Console" to run PythonTA,
+    # and then also test your methods manually in the console.
+    # import python_ta
+    # python_ta.check_all(config={
+    #     'max-line-length': 120,
+    #     'extra-imports': ['random', 'a3_network', 'a3_part1'],
+    #     'disable': ['unused-import']
+    # })
