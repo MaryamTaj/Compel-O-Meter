1ed39a35914a6802d7c203e5936a2ef0743df799
akshayadeepak <akshayad0301@gmail.com>
2023-04-02 23:40:24 -0400

    final updates

---
+++ gui.py
@@ -0,0 +1,76 @@
+""" CSC111 Winter 2023 Course Project : Compel-O-Meter
+
+Description
+===========
+This file contains the functions required to display an interactive user interface :)
+
+Copyright
+==========
+This file is Copyright (c) 2023 Akshaya Deepak Ramachandran, Kashish Mittal, Maryam Taj and Pratibha Thakur
+"""
+
+import tkinter as tk
+import analysis
+
+
+def submit_name(event):
+    """Return the analysis of the text inputted by the user"""
+    # save the text provided by the users
+    text = name_entry.get("1.0", "end-1c")
+
+    # analyse the text
+    results = analysis.compellingness_with_description(text)
+    score = results[0]
+    descrp = results[1], results[2], results[3], results[4], results[5]
+    rtrn_txt = str(score) + '\n' + descrp[0] + "\n" + descrp[1] + "\n" + descrp[2] + "\n" + descrp[3] + "\n" + descrp[4]
+
+    # return an analysis based on the text
+    message_label.config(text=rtrn_txt)
+
+
+# cretaing a window to allow users to interact with the data by allowing them to input text
+window = tk.Tk()
+window.title("Name Entry")
+window_width = 800
+window_height = 800
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window.config(bg="black")
+
+x_pos = (screen_width - window_width) // 2
+y_pos = (screen_height - window_height) // 2
+
+window.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+
+# Set the grid row and column configuration to center all widgets
+window.grid_columnconfigure(0, weight=1)
+window.grid_rowconfigure(1, weight=1)
+
+display_text = "Please enter your text and then click the submit button below:"
+
+# displaying the above text to provide users with instructions on how to use the interactive interface
+name_label = tk.Label(window, text=display_text, font=("Helvetica", 20), bg="black", fg="white")
+name_label.grid(row=0, column=0, padx=10, pady=(20, 10), sticky=tk.N+tk.S+tk.E+tk.W)
+
+# creating a text box for the users to input their text into
+name_entry = tk.Text(window, font=("Helvetica", 20), width=30, height=5, bg="grey")
+name_entry.grid(row=1, column=0, padx=50, pady=10, sticky=tk.N+tk.S+tk.E+tk.W)
+
+# creating the submit button that will allow the users to submit their text and will allow the program to analyse what
+# they have typed
+canvas = tk.Canvas(window, width=150, height=70, highlightthickness=0, bg="black")
+canvas.grid(row=4, column=0, padx=10, pady=10)
+
+rect_button = canvas.create_rectangle(10, 10, 140, 60, fill="black", outline="white", width=4)
+canvas.tag_bind(rect_button, "<Button-1>", submit_name)
+
+submit_button = canvas.create_text(75, 35, text="Submit", font=("Helvetica", 20), fill="white", anchor="center")
+canvas.tag_bind(submit_button, "<Button-1>")
+
+canvas.tag_bind(submit_button, "<Button-1>", submit_name)
+
+message_label = tk.Label(window, text="Analysis will be returned here", font=("Helvetica", 20), bg="grey", fg="white",
+                         wraplength=600)
+message_label.grid(row=5, column=0, padx=10, pady=10, sticky=tk.N+tk.S+tk.E+tk.W)
+
+window.mainloop()
--- read_csv.py
+++ read_csv.py
@@ -1,12 +1,20 @@
-"""Generating the lexicon used for data analysis"""
+""" CSC111 Winter 2023 Course Project : Compel-O-Meter
 
+Description
+===========
+Generating the lexicon used for data analysis
+
+Copyright
+==========
+This file is Copyright (c) 2023 Akshaya Deepak Ramachandran, Kashish Mittal, Maryam Taj and Pratibha Thakur
+"""
 import csv
+from python_ta.contracts import check_contracts
 
-from nltk.corpus import wordnet
 
-
+@check_contracts
 def read_csv_positive_file(csv_file1: str) -> dict[str, int]:
-    """..."""
+    """Takes in a csv file of positive words and returns a dictionary where each word in the file is assigned to 1."""
 
     with open(csv_file1) as file:
         reader = csv.reader(file)
@@ -19,21 +27,12 @@ def read_csv_positive_file(csv_file1: str) -> dict[str, int]:
         words = {}
         for row in reader:
             words[row[0]] = 1
-
-    synonyms = []
-    for word in words:
-        for syn in wordnet.synsets(word):
-            for lemma in syn.lemmas():
-                synonyms.append(lemma.name())
-
-    for syns in synonyms:
-        words[syns] = 1
-
     return words
 
 
+@check_contracts
 def read_csv_negative_file(csv_file1: str) -> dict[str, int]:
-    """..."""
+    """Takes in a csv file of negative words and returns a dictionary where each word in the file is assigned to -1."""
 
     with open(csv_file1) as file:
         reader = csv.reader(file)
@@ -46,21 +45,13 @@ def read_csv_negative_file(csv_file1: str) -> dict[str, int]:
         words = {}
         for row in reader:
             words[row[0]] = -1
-
-    synonyms = []
-    for word in words:
-        for syn in wordnet.synsets(word):
-            for lemma in syn.lemmas():
-                synonyms.append(lemma.name())
-
-    for syns in synonyms:
-        words[syns] = -1
-
     return words
 
 
+@check_contracts
 def return_dictionary(csv_file1: str, csv_file2: str) -> dict[str, int]:
-    """..."""
+    """Takes in 2 csv files,one containing postive words and the other containing negative words. It returns a
+    dictionary containing words from both the files with appropriate setiment scores assigned."""
     positive = read_csv_positive_file(csv_file1)
     negative = read_csv_negative_file(csv_file2)
 
@@ -71,4 +62,27 @@ def return_dictionary(csv_file1: str, csv_file2: str) -> dict[str, int]:
     return words
 
 
-return_dictionary('data/positive_words.csv', 'data/negative.csv')
+@check_contracts
+def reasoning_words_list(csv_file: str) -> list:
+    """ Takes in a csv file of reasoning words and returns a list containing these words."""
+    with open(csv_file) as file:
+        reader = csv.reader(file)
+        reasoning_words = []
+        for row in reader:
+            row = row[0].lower()
+            reasoning_words.append(row)
+    return reasoning_words
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=True)
+
+    import python_ta
+
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'disable': ['forbidden-import'],
+        'allowed-io': ['read_csv_positive_file', 'read_csv_negative_file', 'return_dictionary', 'reasoning_words_list']
+    })
