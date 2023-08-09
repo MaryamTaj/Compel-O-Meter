44e15c4a15f944e47e21a52b37f5f0f0fa4897e5
MaryamTaj <109046900+MaryamTaj@users.noreply.github.com>
2023-08-08 21:47:31 -0400

    Delete loading.js

--- static/loading.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loadingOverlay = document.getElementById('loadingOverlay')
-    loadingOverlay.style.display = 'none'
-
-    window.addEventListener('beforeunload', function() {
-        loadingOverlay.style.display = 'block'
-    })
-})
